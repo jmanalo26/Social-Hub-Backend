749c16d4350cdc732f1f0017a5e6485bf099cb36
Aaron <arrovol47@gmail.com>
2021-01-25 18:09:52 -0600

    Initial commit

---
+++ src/main/java/TheBoyz/TheBoyz/TheBoyzApplication.java
@@ -0,0 +1,14 @@
+package TheBoyz.TheBoyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBoyzApplication {
+
+	public static void main(String[] args) {
+		System.out.println("yo Momma!");
+		SpringApplication.run(TheBoyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TheBoyz/TheBoyz/TheBoyzApplicationTests.java
@@ -0,0 +1,13 @@
+package TheBoyz.TheBoyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBoyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
