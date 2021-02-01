06115cd360c9a52ee870e2f473a628352222c985
Aaron <arrovol47@gmail.com>
2021-01-31 20:05:27 -0600

    twitter api

--- src/main/java/TheBoyz/TheBoyz/data/repository/UserRepository.java
+++ src/main/java/TheBoyz/TheBoyz/data/repository/UserRepository.java
@@ -1,12 +1,8 @@
-package TheBoyz.TheBoyz.data.repository;
-
-import TheBoyz.TheBoyz.data.model.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-   public Page<User> findByUserIdContainingOrderByUserId(Long userId, Pageable pageable);
-
-   public User findUserByUsername(String username);
-}
+//package TheBoyz.TheBoyz.data.repository;
+//
+//import TheBoyz.TheBoyz.data.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//
+//}
