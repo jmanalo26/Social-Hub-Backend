46b70e79e55d9b22770c03b8ca78427bcaeea5d7
Aaron <arrovol47@gmail.com>
2021-02-08 14:52:59 -0600

    connection to front end

--- src/main/java/TheBoyz/TheBoyz/web/controller/TwitterController.java
+++ src/main/java/TheBoyz/TheBoyz/web/controller/TwitterController.java
@@ -4,6 +4,7 @@ import TheBoyz.TheBoyz.web.service.TwitterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import twitter4j.TwitterException;
@@ -18,6 +19,7 @@ public class TwitterController {
     public TwitterController(final TwitterService twitterService){
         this.twitterService = twitterService;
     }
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/api/twitter/followers")
     public ResponseEntity<Integer> getFollowerCount() throws TwitterException {
        return new ResponseEntity<>(twitterService.getFollowerCount(), HttpStatus.OK);
